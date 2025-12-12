9dd99126c732bd0bcbeadfac9f1a5223c05aaa96
Vinothkumar <vinothkumarvk6669@gmail.com>
2025-12-12 11:13:00 +0530

    finalize toast system with listener cleanup and safe open state

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -98,3 +98,52 @@ export const reducer = (state: State, action: any): State => {
   }
 }
 
+export const reducer = (state: State, action: any): State => {
+  switch (action.type) {
+    case "ADD_TOAST":
+      return { ...state, toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT) }
+    case "UPDATE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === action.toast.id ? { ...t, ...action.toast } : t
+        ),
+      }
+    case "DISMISS_TOAST": {
+      const { toastId } = action
+      if (toastId) addToRemoveQueue(toastId)
+      else state.toasts.forEach((t) => addToRemoveQueue(t.id))
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === toastId || toastId === undefined ? { ...t, open: false } : t
+        ),
+      }
+    }
+    case "REMOVE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.filter((t) => t.id !== action.toastId),
+      }
+    default:
+      return state
+  }
+}
+
+function useToast() {
+  const [state, setState] = React.useState<State>(memoryState)
+  React.useEffect(() => {
+    listeners.push(setState)
+    return () => {
+      const index = listeners.indexOf(setState)
+      if (index > -1) listeners.splice(index, 1)
+    }
+  }, [])
+  return {
+    ...state,
+    toast,
+    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+  }
+}
+
+export { useToast, toast }
