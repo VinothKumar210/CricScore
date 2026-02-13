bd22a3bd09dbfda57e0c261cb8279001e3ca00a6
VinothKumar210 <vinothkumarvk6669@gmail.com>
2026-02-13 15:32:37 +0530

    prevent vite config bundling

--- server/vite.ts
+++ server/vite.ts
@@ -1,13 +1,14 @@
 import express, { type Express } from "express";
-import type { Express } from "express";
+import fs from "fs";
+import path from "path";
 import type { Server } from "http";
+import { nanoid } from "nanoid";
 import { log } from "./logging";
-
-
 
 export async function setupVite(app: Express, server: Server) {
   const { createServer: createViteServer, createLogger } = await import("vite");
-  const viteConfig = (await import("../vite.config")).default;
+  const viteConfigPath = "../vite.config";
+  const viteConfig = (await import(viteConfigPath)).default;
   const viteLogger = createLogger();
 
   const serverOptions = {
